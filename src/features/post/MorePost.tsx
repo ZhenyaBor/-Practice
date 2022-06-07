5322f7f90fbce29bd1762751f6d718bccce848ea
Сергей Мельничук <Melnichuk@eds.local>
2022-06-07 16:00:17 +0300

    ts

--- src/features/post/MorePost.tsx
+++ src/features/post/MorePost.tsx
@@ -1,27 +1,10 @@
-/** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
-import { usePost } from "./hooks/usePost";
+import { PostItems } from "./components/PostItem";
+
 
 export const MorePost = () => {
-    const { posts }:any = usePost();
+   
     return (
-      <div
-      css={css`
-      border: solid 1px black;
-      padding: 10px;
-      background-color: #c0c0a5;
-      border-radius: 10px;
-      font-size: 25px;
-    `}
-      >
-          <p><span
-                css={css`
-                color:red;
-              `}
-          >{posts.title}</span>: {posts.body}</p>
-         <div> {posts.tags}</div>
-        
-      </div>
+      <PostItems/>
     );
   };
   
---
+++ src/features/post/components/PostItem.tsx
@@ -0,0 +1,28 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { usePost } from "../hooks/usePost";
+  
+export const PostItems = () => {
+
+  const { posts }:any = usePost();
+
+  return(
+    <div
+    css={css`
+    border: solid 1px black;
+    padding: 10px;
+    background-color: #c0c0a5;
+    border-radius: 10px;
+    font-size: 25px;
+  `}
+    >
+        <p><span
+              css={css`
+              color:red;
+            `}
+        >{posts.title}</span>: {posts.body}</p>
+       <div> {posts.tags}</div>
+      
+    </div>
+  )
+}
